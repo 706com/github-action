430b6fd1fdad1cac4c8e42a24acc759e3c383564
706com <ehdgusjdbswl@gmail.com>
2024-08-09 16:27:34 +0900

    [Init] git init

---
+++ src/test/java/com/example/dev01/Dev01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dev01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dev01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
